ce0990cf3e509c8c9fc9e1bf072cb97b73461d02
Manx <33370006+manxoh@users.noreply.github.com>
2025-08-04 18:11:14 +0100

    Added axis_sink.v
    Just  a placeholder

---
+++ axis_example/axis_sink.v
@@ -0,0 +1,16 @@
+module axis_sink #(AXIS_WIDTH = 32)
+                                  (input clk,
+                                   input reset,
+                                   input s_axis_tvalid,
+                                   input [AXIS_WIDTH-1:0] s_axis_tdata,
+                                   output s_axis_tready,
+                                   output valid,
+                                   output [AXIS_WIDTH-1:0] data_out);
+
+   //internal signals
+
+   //modify data
+
+   //outputs
+
+endmodule
