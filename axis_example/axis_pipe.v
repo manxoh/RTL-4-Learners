b9789017a2466dce8e1f6530d0552e5878a0605c
Manx <33370006+manxoh@users.noreply.github.com>
2025-08-04 18:10:14 +0100

    Added axis_pipe.v

---
+++ axis_example/axis_pipe.v
@@ -0,0 +1,17 @@
+module axis_pipe #(AXIS_WIDTH = 32)
+                                  (input clk,
+                                   input reset,
+                                   input s_axis_tvalid,
+                                   input [AXIS_WIDTH-1:0] s_axis_tdata,
+                                   output s_axis_tready,
+                                   output m_axis_tvalid,
+                                   output [AXIS_WIDTH-1:0] m_axis_tdata,
+                                   input m_axis_tready);
+
+   //internal signals
+
+   //modify data
+
+   //outputs
+
+endmodule
