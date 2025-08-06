809ce57e91a78e2c4bf4babb5231f34d795e9c14
Manx <33370006+manxoh@users.noreply.github.com>
2025-08-06 17:53:51 +0100

    Update tb_axis_ex.v

--- axis_example/tb_axis_ex.v
+++ axis_example/tb_axis_ex.v
@@ -7,18 +7,36 @@ reg reset;
 reg en;
 reg [AXIS_WIDTH-1:0] data = {AXIS_WIDTH{1'b0}};
 wire m_axis_tvalid;
+wire s_axis_tvalid;
 wire [AXIS_WIDTH-1:0] m_axis_tdata;
-reg m_axis_tready;
+wire [AXIS_WIDTH-1:0] s_axis_tdata;
+wire s_axis_tready;
 
-axis_source #(AXIS_WIDTH) dut (.clk(clk),
+axis_source #(AXIS_WIDTH) axi_source_inst (.clk(clk),
                                                .reset(reset),
                                                 .en(en),
-                                                .data(data),
-                                                .m_axis_tvalid(m_axis_tvalid),
-                                                .m_axis_tdata(m_axis_tdata),
-                                                .m_axis_tready(m_axis_tready)
-                                          );
+                                                .init_data(data),
+                                                .m_axis_tvalid(s_axis_tvalid),
+                                                .m_axis_tdata(s_axis_tdata),
+                                                .m_axis_tready(s_axis_tready)
+                                                );
 
+axis_pipe #(AXIS_WIDTH) axi_pipe_inst (.clk(clk),
+                                                                .reset(reset),
+                                                               .s_axis_tvalid(s_axis_tvalid),
+                                                               .s_axis_tdata(s_axis_tdata),
+                                                               .s_axis_tready(s_axis_tready),
+                                                               .m_axis_tvalid(m_axis_tvalid),
+                                                               .m_axis_tdata(m_axis_tdata),
+                                                               .m_axis_tready(m_axis_tready)
+                                                               );
+
+axis_sink #(AXIS_WIDTH) axi_sink_inst (.clk(clk),
+                                                                .reset(reset),
+                                                               .s_axis_tvalid(m_axis_tvalid),
+                                                               .s_axis_tdata(m_axis_tdata),
+                                                               .s_axis_tready(m_axis_tready)
+                                                               );
 
 initial begin
    clk <= 1'b0;
@@ -33,15 +51,10 @@ initial begin
    # 10 en = 1'b1;
       data = 1'b1;
    # 250 en = 1'b0;
-   #20 $finish;
-end
-
-initial begin
-   m_axis_tready <= 1'b0;
-   forever begin
-      @ (posedge clk);
-       m_axis_tready <= ~m_axis_tready;
-   end
+   # 60 en = 1'b1;
+      data = 4'b1001;
+   # 250 en = 1'b0;
+   #60 $finish;
 end
 
 endmodule
